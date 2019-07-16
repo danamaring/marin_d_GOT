cdc24579f4b9cbece1476c648e51efcaafacf4b2
Marin Garcia <d_maringarcia@tss-hr420-dm22.fanshawec.ca>
2019-07-16 17:49:01 -0400

    carousel

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -4,7 +4,8 @@
   const sigils = document.querySelectorAll('.sigilContainer'),
         lightBox = document.querySelector('.lightbox'),
         closeButton = document.querySelector('.close-lightbox'),
-        houseVideo = document.querySelector('.house-video');
+        houseVideo = document.querySelector('.house-video'),
+        bannerImages = document.querySelector('#houseImages');
 
   function popLightBox() {
   //make the lightbox show up
@@ -21,7 +22,22 @@
   houseVideo.pause();
 }
 
-  sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
+function animateBanners() {
+  //we need an offset that we can multiply by to animate
+  //our banners to the left and make the active one show up
+
+  let offset = 600,
+      multiplier = this.dataset.offset;
+      //this is the data-offset custom data attribute on each of the sigils
+  console.log((offset * multiplier) + "px");
+
+  //move the banners to the left using the product of our math
+  bannerImages.style.right = `${offset * multiplier + "px"}`;
+}
+
+  // sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
+  sigils.forEach(sigil => sigil.addEventListener("click", animateBanners));
+
   closeButton.addEventListener("click", closeLightBox);
 
   houseVideo.addEventListener('ended', closeLightBox);
