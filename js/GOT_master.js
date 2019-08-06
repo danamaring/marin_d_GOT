ad826443e5bdb4ef4ffead88a32458f2ac48e9c3
Dana Marin <d_maringarcia@fanshaweonline.ca>
2019-08-06 00:59:53 -0400

    adding more houses, arrays, and video controls

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -10,7 +10,8 @@
         houseInfo = document.querySelector(".house-info"),
         pauseButton = document.querySelector(".fa-pause-circle"),
         playButton = document.querySelector(".fa-play-circle"),
-        rewindButton = document.querySelector(".fa-backward");
+        forwardButton = document.querySelector(".fa-forward"),
+        replayButton = document.querySelector(".fa-redo-alt");
 
         //houseData is a multidimensional array (arrays within arryas!) Data container can hold anything - in this casem each
         //index or entry holds another, smaller container with 2 indexes - 1 with the house name, one with the house data.
@@ -30,22 +31,33 @@
     ["greyjoy", `House Greyjoy of Pyke is one of the Great Houses of Westeros. It rules over the Iron Islands, a harsh and bleak collection of islands off the west coast of Westeros, from the castle at Pyke. The head of the house is the Lord Reaper of Pyke.House Greyjoy's sigil is traditionally a golden kraken on a black field. Their house words are "We Do Not Sow," although the phrase "What Is Dead May Never Die" is also closely associated with House Greyjoy and their bannermen, as they are associated with the faith of the Drowned God. `],
     //houseData[5][0] is "lannister", houseData [5][1] is the house data
     ["arryn", `House Arryn of the Eyrie is one of the Great Houses of Westeros. It has ruled over the Vale of Arryn for millennia, originally as the Kings of Mountain and Vale and more recently as Lords Paramount of the Vale and Wardens of the East under the Targaryen kings and Baratheon-Lannister kings. The nominal head of House Arryn is Robin Arryn, the Lord of the Eyrie, with his stepfather Petyr Baelish acting as Lord Protector until he reaches the age of majority.`]
+    //houseData[6][0] is "lannister", houseData [5][1] is the house data
+    ["targayen", `House Targaryen of Dragonstone is a Great House of Westeros and was the ruling royal House of the Seven Kingdoms for three centuries since it conquered and unified the realm, before it was deposed during Robert's Rebellion and House Baratheon replaced it as the new royal House. The few surviving Targaryens fled into exile to the Free Cities of Essos across the Narrow Sea. Currently based on Dragonstone off of the eastern coast of Westeros, House Targaryen seeks to retake the Seven Kingdoms from House Lannister, who formally replaced House Baratheon as the royal House following the destruction of the Great Sept of Baelor.`]
+    //houseData[7][0] is "lannister", houseData [5][1] is the house data
+    ["frey", `House Frey of the Twins was the Great House of the Riverlands, having gained their position for their treachery against their former liege lords, House Tully, who were stripped of all their lands and titles for their rebellion against the Iron Throne; House Tully had supported the independence movement for the Kingdom of the North. The current head of the house is unknown following the assassinations of Lord Walder Frey and two of his sons, Lothar Frey and Walder Rivers, by the vengeful Arya Stark. This is made more complex by the subsequent assassination of all the male Freys soon after.`]
+    //houseData[8][0] is "lannister", houseData [5][1] is the house data
+    ["tyrell", `House Tyrell of Highgarden is an extinct Great House of Westeros. It ruled over the Reach, a vast, fertile, and heavily-populated region of southwestern Westeros, from their castle-seat of Highgarden as Lords Paramount of the Reach and Wardens of the South after taking control of the region from House Gardener during the Targaryen conquest.`]
   ];
 
-  //pause the video on a click
-
-  function pauseVideo() {
-    //pause the video when the button is clicked
-    houseVideo.pause()
-  }
-
-  function playVideo() {
-    houseVideo.play()
-  }
-
-  function rewindVideo() {
-    houseVideo.loop()
-  }
+//VIDEO CONTROLS
+  // function pauseVideo() {
+  //   //pause the video when the button is clicked
+  //   houseVideo.pause()
+  // }
+  // 
+  // function playVideo() {
+  //   houseVideo.playbackRate = 1;
+  //   houseVideo.play()
+  // }
+  //
+  // function forwardVideo() {
+  //   houseVideo.playbackRate = 7;
+  // }
+  //
+  // function replayVideo() {
+  //   houseVideo.currentTime = 0;
+  //   houseVideo.play()
+  // }
 
   //write the other functions for the custom video controls (play, colume, control, time counter, progress bar scrubber, etc.)
 
@@ -104,5 +116,6 @@ function animateBanners() {
   houseVideo.addEventListener('ended', closeLightBox);
   pauseButton.addEventListener("click", pauseVideo);
   playButton.addEventListener("click", playVideo);
-  rewindButton.addEventListener("click", rewindVideo);
+  forwardButton.addEventListener("click", forwardVideo);
+  replayButton.addEventListener("click", replayVideo);
 })();
